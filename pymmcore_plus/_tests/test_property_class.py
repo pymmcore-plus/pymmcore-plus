8fb95c903b4eb20a32e9893b3c762d5879508db4
Talley Lambert <talley.lambert@gmail.com>
2022-03-07 18:48:03 -0500

    add test

--- pymmcore_plus/_tests/test_property_class.py
+++ pymmcore_plus/_tests/test_property_class.py
@@ -1,6 +1,8 @@
+from unittest.mock import Mock
+
 import pytest
 
-from pymmcore_plus import CMMCorePlus
+from pymmcore_plus import CMMCorePlus, DeviceProperty
 
 
 def test_mmproperty(core: CMMCorePlus):
@@ -11,3 +13,11 @@ def test_mmproperty(core: CMMCorePlus):
         if prop.isReadOnly():
             with pytest.warns(UserWarning):
                 prop.value = "asdf"
+
+
+def test_property_callbacks(core: CMMCorePlus):
+    prop = DeviceProperty(core, "Camera", "Gain")
+    mock = Mock()
+    prop.connect_change_callback(mock)
+    prop.value = 6
+    mock.assert_called_once_with("6")
