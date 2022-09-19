ce2ed31c672e25baa827ba69d3579202f5cfee43
Talley Lambert <talley.lambert@gmail.com>
2022-09-19 14:40:12 -0400

    skip lock and call on CI windows

--- pymmcore_plus/_tests/test_core.py
+++ pymmcore_plus/_tests/test_core.py
@@ -416,6 +416,9 @@ def test_guess_channel_group(core: CMMCorePlus):
         assert chan_group == ["Channel"]
 
 
+@pytest.mark.skipif(
+    os.name == "nt" and os.getenv("CI"), reason="CI on windows is broken"
+)
 def test_lock_and_callbacks(core: CMMCorePlus, qtbot):
     if not isinstance(core.events, QObject):
         pytest.skip(reason="Skip lock tests on psygnal until we can remove qtbot.")
