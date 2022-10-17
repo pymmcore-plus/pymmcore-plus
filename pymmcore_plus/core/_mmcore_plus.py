da277f28a9ae1b14064c4584d18b8cbd63de9ffc
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2022-10-17 16:00:17 -0400

    feat: new signals to handle pixel size configurations (#143)
    * feat: add pixelSizeDefined signal
    
    * fix: fix new signal
    
    * feat: add more signals
    
    * feat: update definePixelSizeConfig
    
    * fix: update definePixelSizeConfig
    
    * fix: fix spelling
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * test: add tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: remove signals
    
    * fix: update test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update pymmcore_plus/core/_mmcore_plus.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- pymmcore_plus/core/_mmcore_plus.py
+++ pymmcore_plus/core/_mmcore_plus.py
@@ -724,6 +724,28 @@ class CMMCorePlus(pymmcore.CMMCore):
             state = args
         self.events.propertyChanged.emit(shutterLabel, "State", state)
 
+    def setPixelSizeUm(self, resolutionID: str, pixSize: float) -> None:
+        super().setPixelSizeUm(resolutionID, pixSize)
+        self.events.pixelSizeChanged.emit(pixSize)
+
+    def deletePixelSizeConfig(self, resolutionID: str):
+        super().deletePixelSizeConfig(resolutionID)
+        self.events.pixelSizeChanged.emit(0.0)
+
+    @overload
+    def definePixelSizeConfig(self, resolutionID: str) -> None:
+        ...
+
+    @overload
+    def definePixelSizeConfig(
+        self, resolutionID: str, deviceLabel: str, propName: str, value: str
+    ) -> None:
+        ...
+
+    def definePixelSizeConfig(self, *args) -> None:
+        super().definePixelSizeConfig(*args)
+        self.events.pixelSizeChanged.emit(0.0)
+
     @overload
     def setROI(self, x: int, y: int, width: int, height: int) -> None:
         ...  # pragma: no cover
