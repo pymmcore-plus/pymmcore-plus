c83472b9980b0c6ce74043c01843734cdd7232ef
Talley Lambert <talley.lambert@gmail.com>
2023-06-16 15:45:39 -0400

    chore: remove test case

--- src/pymmcore_plus/core/_mmcore_plus.py
+++ src/pymmcore_plus/core/_mmcore_plus.py
@@ -1193,13 +1193,14 @@ class CMMCorePlus(pymmcore.CMMCore):
         """
         autoshutter = self.getAutoShutter()
         if autoshutter:
-            shutter_dev = self.getShutterDevice()
-            self.events.propertyChanged.emit(shutter_dev, "State", True)
+            self.events.propertyChanged.emit(self.getShutterDevice(), "State", True)
         try:
             super().snapImage()
         finally:
             if autoshutter:
-                self.events.propertyChanged.emit(shutter_dev, "State", False)
+                self.events.propertyChanged.emit(
+                    self.getShutterDevice(), "State", False
+                )
 
     @property
     def mda(self) -> MDARunner:
