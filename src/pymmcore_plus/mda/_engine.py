8bad5018ed1205a54601e6ff7d8aef44cef3b2fe
fdrgsp <federico.gasparoli@gmail.com>
2023-06-18 22:52:04 -0400

    fix: update engine

--- src/pymmcore_plus/mda/_engine.py
+++ src/pymmcore_plus/mda/_engine.py
@@ -22,8 +22,7 @@ class MDAEngine(PMDAEngine):
     def __init__(self, mmc: CMMCorePlus) -> None:
         self._mmc = mmc
 
-        self._z_start = 0  # used for one_shot autofocus
-        self._current_pos = 0  # used for one_shot autofocus
+        self._correction = 0  # used for one_shot autofocus
 
     def setup_sequence(self, sequence: MDASequence) -> None:
         """Setup the hardware for the entire sequence.
@@ -34,8 +33,6 @@ class MDAEngine(PMDAEngine):
         from ..core import CMMCorePlus
 
         self._mmc = self._mmc or CMMCorePlus.instance()
-
-        self._z_plan = list(sequence.z_plan)
 
     def setup_event(self, event: MDAEvent) -> None:
         """Set the system hardware (XY, Z, channel, exposure) as defined in the event.
@@ -51,80 +48,29 @@ class MDAEngine(PMDAEngine):
             self._mmc.setXYPosition(x, y)
 
         if event.z_pos is not None:
-            if event.properties:
-                z_device, z_af_device, z_af, use_af = self._get_properties_values(
-                    event.properties
-                )
-                channel_offset = event.channel.z_offset
-                channel_do_stack = event.channel.do_stack
+            
+            # TODO: exclude when absolute z_plan
+            if event.autofocus is not None:
+                z_af_device, z_af_pos = event.autofocus
 
-                # if z_plan
                 if len(event.sequence.z_plan) > 1:
-                    z_idx = event.index["z"]
+                    # if first frame of z stack, calculate the correction
+                    if event.index["z"] == 0:
+                        z_after_af = self._execute_autofocus(z_af_device, z_af_pos)
+                        # the first z event is the top of the stack, to know that
+                        # is the starting z position we need to subtract the first
+                        # z offset from the relative z plan (self._z_plan[0])
+                        first_pos = event.z_pos -  list(event.sequence.z_plan)[0]
+                        # calculate the correction to apply to each z position
+                        self._correction = z_after_af - first_pos
 
-                    if not use_af:
-                        self._mmc.setZPosition(event.z_pos)
+                    self._mmc.setZPosition(event.z_pos + self._correction)
 
-                    elif z_idx == 0:
-                        # set autofocus position
-                        self._mmc.setPosition(z_af_device, z_af)
-                        self._mmc.fullFocus()
-
-                        self._mmc.setPosition(z_device, 250)  # to test with demo cfg
-
-                        # get current resulting z position using z focus device
-                        # and add any channel offset
-                        self._z_start = (
-                            self._mmc.getPosition(z_device) + channel_offset
-                            if channel_offset
-                            else self._mmc.getPosition(z_device)
-                        )
-
-                        # add z_plan offset to current z position or use z_start
-                        # if not channel_do_stack
-                        self._current_pos = (
-                            self._z_start + self._z_plan[0]
-                            if channel_do_stack
-                            else self._z_start
-                        )
-    
-                        self._mmc.setPosition(z_device, self._current_pos)
-
-                    else:
-                        # if not first frame, just add zplan offset.
-                        # if offset is 0, use z_start
-                        self._current_pos = (
-                            self._z_start
-                            if self._z_plan[z_idx] == 0
-                            else self._current_pos + self._z_plan[z_idx]
-                        )
-                        self._mmc.setPosition(z_device, self._current_pos)
-
-                # if no z_plan
-                elif len(event.sequence.z_plan) == 0:
-                    if not use_af:
-                        self._mmc.setZPosition(event.z_pos)
-
-                    else:
-                        # set autofocus position
-                        self._mmc.setPosition(z_af_device, z_af)
-                        self._mmc.fullFocus()
-
-                        self._mmc.setPosition(z_device, 250)  # to test with demo cfg
-
-                        # add any channel offset
-                        if channel_offset:
-                            self._mmc.setPosition(
-                                z_device, self._mmc.getPosition(z_device) + channel_offset
-                            )
-
-                else:
-                    self._mmc.setZPosition(event.z_pos)
-
+                else:  # no z or len(z_plan) == 1
+                    z_after_af = self._execute_autofocus(z_af_device, z_af_pos)
+                    self._mmc.setZPosition(z_after_af)
             else:
                 self._mmc.setZPosition(event.z_pos)
-
-        print("curr:", self._mmc.getPosition())
 
         if event.channel is not None:
             self._mmc.setConfig(event.channel.group, event.channel.config)
@@ -138,33 +84,12 @@ class MDAEngine(PMDAEngine):
         # TODO: add non-aquisition event-specific logic here later
         self._mmc.snapImage()
         return EventPayload(image=self._mmc.getImage())
-
-    def _get_properties_values(
-        self, properties: list[PropertyTuple]
-    ) -> tuple[str | None, str | None, float | None, bool | None]:
-        """Get the values of the properties that are used for one_shot autofocus."""
-        z_device, z_af_device, z_af, use_af = None, None, None, None
-
-        for prop in properties:
-            if prop.device_name == "z_device" and prop.property_name == "device_name":
-                z_device = prop.property_value
-            elif (
-                prop.device_name == "z_autofocus_device"
-                and prop.property_name == "device_name"
-            ):
-                z_af_device = prop.property_value
-            elif (
-                prop.device_name == "z_autofocus_device"
-                and prop.property_name == "position"
-            ):
-                z_af = prop.property_value
-            elif (
-                prop.device_name == "z_autofocus_device"
-                and prop.property_name == "state"
-            ):
-                use_af = prop.property_value
-
-        return z_device, z_af_device, z_af, use_af
+    
+    def _execute_autofocus(self, z_af_device_name, z_af_pos) -> float:
+        """Perform the autofocus."""
+        self._mmc.setPosition(z_af_device_name, z_af_pos)
+        self._mmc.fullFocus()
+        return self._mmc.getZPosition()
 
 
 class EventPayload(NamedTuple):
