f48205ef26c1e84a5835b775afdb437a39f81a5e
Talley Lambert <talley.lambert@gmail.com>
2023-07-25 14:15:18 -0400

    update mock

--- src/pymmcore_plus/_mock_sample.py
+++ src/pymmcore_plus/_mock_sample.py
@@ -3,21 +3,27 @@ from __future__ import annotations
 from contextlib import AbstractContextManager, ContextDecorator
 from functools import wraps
 from types import TracebackType
-from typing import TYPE_CHECKING, Any, Callable, Iterator, overload
+from typing import TYPE_CHECKING, Any, Callable, Generic, Iterator, TypeVar, overload
 from unittest.mock import patch
 
 if TYPE_CHECKING:
+    from unittest.mock import _patch
+
     import numpy as np
     from pymmcore import CMMCore
     from typing_extensions import Literal, ParamSpec
 
-    _P = ParamSpec("_P")
+    P = ParamSpec("P")
+    
+R = TypeVar("R")
 
 
-class _MockSampleContextManager(AbstractContextManager, ContextDecorator):
+class _CorePatcher(AbstractContextManager, ContextDecorator, Generic[R]):
+    """Context manager that patches the provided (or global) mmcore object."""
+
     def __init__(
         self,
-        image_generator: Callable[_P, Iterator[np.ndarray]],
+        func: Callable[P, Iterator[R]],
         args: tuple[Any, ...],
         kwds: dict[str, Any],
         loop: bool = True,
@@ -25,64 +31,79 @@ class _MockSampleContextManager(AbstractContextManager, ContextDecorator):
     ) -> None:
         from pymmcore_plus import CMMCorePlus
 
-        self._mmcore = mmcore or CMMCorePlus.instance()
+        self._core = mmcore or CMMCorePlus.instance()
         self._loop = loop
-        self._image_generator = image_generator
 
-        self.gen = image_generator(*args, **kwds)
-        self.func, self.args, self.kwds = image_generator, args, kwds
-        self._get_patcher = patch.object(self._mmcore, "getImage", self._getImage)
-        self._snap_patcher = patch.object(self._mmcore, "snapImage", self._snapImage)
+        self.func, self.args, self.kwds = func, args, kwds
+        self.gen = self._make_new_generator()
+
+        self._patchers: list[_patch] = []
+        for attr in dir(self):
+            if hasattr(self._core, attr):
+                patcher = patch.object(self._core, attr, getattr(self, attr))
+                self._patchers.append(patcher)
 
         # ensure context manager instances have good docstrings
-        doc = getattr(image_generator, "__doc__", None)
+        doc = getattr(func, "__doc__", None)
         if doc is None:
             doc = type(self).__doc__
         self.__doc__ = doc
 
-    def _snapImage(self, *args: Any, **kwargs: Any) -> None:
-        # not currently used, but could be.
-        ...
-
-    def _getImage(self, numChannel: int | None = None, **kwargs: Any) -> np.ndarray:
-        try:
-            return next(self.gen)
-        except StopIteration:
-            if self._loop:
-                self.gen = self._image_generator()
-                return next(self.gen)
-            else:
-                raise RuntimeError("generator didn't yield") from None
+    def _make_new_generator(self) -> Iterator[R]:
+        return self.func(*self.args, **self.kwds)
 
     def _recreate_cm(self) -> _MockSampleContextManager:
-        # _GCMB instances are one-shot context managers, so the
-        # CM must be recreated each time a decorated function is
+        # This overrides the ContextDecorator._recreate_cm method.
+        # _MockSampleContextManager instances are one-shot context managers, so the
+        # context manager must be recreated each time a decorated function is
         # called
         return self.__class__(self.func, self.args, self.kwds)
+
+    def start(self) -> None:
+        """Start all patchers."""
+        for patcher in self._patchers:
+            patcher.start()
+
+    def stop(self) -> None:
+        """Stop all patchers."""
+        for patcher in self._patchers:
+            patcher.stop()
 
     def __enter__(self) -> None:
         # do not keep args and kwds alive unnecessarily
         # they are only needed for recreation, which is not possible anymore
         del self.args, self.kwds, self.func
-        self._get_patcher.start()
-        self._snap_patcher.start()
-        return None
+        self.start()
 
     def __exit__(
         self,
-        __exc_type: type[BaseException] | None,
-        __exc_value: BaseException | None,
-        __traceback: TracebackType | None,
-    ) -> bool | None:
-        self._get_patcher.stop()
-        self._snap_patcher.stop()
-        return None
+        _exc_type: type[BaseException] | None,
+        _exc_value: BaseException | None,
+        _traceback: TracebackType | None,
+    ) -> None:
+        self.stop()
+
+
+class _MockSampleContextManager(_CorePatcher['np.ndarray']):
+    def snapImage(self, *args: Any, **kwargs: Any) -> None:
+        # not currently used, but could be.
+        ...
+
+    def getImage(self, numChannel: int | None = None, **kwargs: Any) -> np.ndarray:
+        try:
+            return next(self.gen)
+        except StopIteration:
+            if self._loop:
+                self.gen = self._make_new_generator()
+                return next(self.gen)
+            else:
+                raise RuntimeError("generator didn't yield") from None
 
 
 @overload
 def mock_sample(
-    func: Callable[_P, Iterator[np.ndarray]]
-) -> Callable[_P, _MockSampleContextManager]:
+    func: Callable[P, Iterator[np.ndarray]]
+) -> Callable[P, _MockSampleContextManager]:
     ...
 
 
@@ -93,20 +114,20 @@ def mock_sample(
     loop: bool = ...,
     mmcore: CMMCore | None = ...,
 ) -> Callable[
-    [Callable[_P, Iterator[np.ndarray]]], Callable[_P, _MockSampleContextManager]
+    [Callable[P, Iterator[np.ndarray]]], Callable[P, _MockSampleContextManager]
 ]:
     ...
 
 
 def mock_sample(
-    func: Callable[_P, Iterator[np.ndarray]] | None = None,
+    func: Callable[P, Iterator[np.ndarray]] | None = None,
     *,
     loop: bool = True,
     mmcore: CMMCore | None = None,
 ) -> (
-    Callable[_P, _MockSampleContextManager]
+    Callable[P, _MockSampleContextManager]
     | Callable[
-        [Callable[_P, Iterator[np.ndarray]]], Callable[_P, _MockSampleContextManager]
+        [Callable[P, Iterator[np.ndarray]]], Callable[P, _MockSampleContextManager]
     ]
 ):
     """Decorator to create a context manager that mocks the core's getImage method.
@@ -156,8 +177,8 @@ def mock_sample(
     """
 
     def _decorator(
-        func: Callable[_P, Iterator[np.ndarray]]
-    ) -> Callable[_P, _MockSampleContextManager]:
+        func: Callable[P, Iterator[np.ndarray]]
+    ) -> Callable[P, _MockSampleContextManager]:
         @wraps(func)
         def helper(*args: Any, **kwds: Any) -> _MockSampleContextManager:
             return _MockSampleContextManager(func, args, kwds, loop=loop, mmcore=mmcore)
