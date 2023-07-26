783d590fa306c97dcd5246b845d7cee6443fd6b5
Talley Lambert <talley.lambert@gmail.com>
2023-07-26 13:27:44 -0400

    test: mock fully sequenceable core

--- tests/test_sequencing.py
+++ tests/test_sequencing.py
@@ -1,14 +1,11 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING, cast
+from math import prod
+from typing import cast
 from unittest.mock import MagicMock, call
 
 import useq
-from pymmcore_plus.core._sequencing import get_all_sequenceable
+from pymmcore_plus import CMMCorePlus
+from pymmcore_plus.core._sequencing import SequencedEvent, get_all_sequenceable
 from pymmcore_plus.mda import MDAEngine, MDARunner
-
-if TYPE_CHECKING:
-    from pymmcore_plus import CMMCorePlus
 
 
 def test_get_all_sequencable(core: CMMCorePlus) -> None:
@@ -52,3 +49,46 @@ def test_sequenced_mda(core: CMMCorePlus) -> None:
 
     expected_pos = [call(0, 0), call(0, 0), call(1, 1), call(1, 1)]
     assert core_mock.setXYPosition.call_args_list == expected_pos
+
+
+def test_fully_sequenceable_core():
+    mda = useq.MDASequence(
+        stage_positions=[(0, 0, 0), (1, 1, 1)],
+        z_plan=useq.ZRangeAround(range=3, step=1),
+        channels=[
+            useq.Channel(config="DAPI", exposure=5),
+            useq.Channel(config="FITC", exposure=10),
+        ],
+        time_plan=useq.TIntervalLoops(interval=0, loops=3),
+    )
+
+    CAM = "Camera"
+    XYSTAGE = "XYStage"
+    FOCUS = "Z"
+    core_mock = cast("CMMCorePlus", MagicMock(spec=CMMCorePlus))
+    core_mock.isSequenceRunning.return_value = False
+    core_mock.isBufferOverflowed.return_value = False
+    core_mock.getCameraDevice.return_value = CAM
+    core_mock.getXYStageDevice.return_value = XYSTAGE
+    core_mock.getFocusDevice.return_value = FOCUS
+    core_mock.getFocusDevice.return_value = FOCUS
+
+    engine = MDAEngine(mmc=core_mock)
+
+    combined_events = list(engine.event_iterator(mda))
+    assert len(combined_events) == 1
+    seq_event = combined_events[0]
+    assert isinstance(seq_event, SequencedEvent)
+
+    runner = MDARunner()
+    runner.set_engine(engine)
+    runner.run(mda)
+
+    n_img = prod(mda.shape)
+    core_mock.startSequenceAcquisition.assert_called_once_with(n_img, 0, True)
+    core_mock.loadExposureSequence.assert_called_once_with(
+        CAM, seq_event.exposure_sequence
+    )
+    core_mock.loadXYStageSequence.assert_called_once_with(
+        XYSTAGE, seq_event.x_sequence, seq_event.y_sequence
+    )
