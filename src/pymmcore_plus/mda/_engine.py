3b4d70e30db9e8fad6ffda54a520c12a902173f3
Talley Lambert <talley.lambert@gmail.com>
2023-07-28 09:30:12 -0400

    feat: add flag for optional sequencing

--- src/pymmcore_plus/mda/_engine.py
+++ src/pymmcore_plus/mda/_engine.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import time
 from typing import TYPE_CHECKING, Iterable, Iterator, NamedTuple, Sequence, cast
 
-import useq
-from useq import MDAEvent, MDASequence
+from useq import HardwareAutofocus, MDAEvent, MDASequence
 
 from pymmcore_plus._logger import logger
 from pymmcore_plus._util import retry
@@ -14,11 +13,8 @@ from ._protocol import PMDAEngine
 
 if TYPE_CHECKING:
     import numpy as np
-    from useq import HardwareAutofocus
 
     from pymmcore_plus.core import CMMCorePlus
-
-HardwareAFType = getattr(useq, "HardwareAutofocus", ())  # useq v0.3.0+
 
 
 class MDAEngine(PMDAEngine):
@@ -26,26 +22,43 @@ class MDAEngine(PMDAEngine):
 
     This implements the [`PMDAEngine`][pymmcore_plus.mda.PMDAEngine] protocol, and
     uses a [`CMMCorePlus`][pymmcore_plus.CMMCorePlus] instance to control the hardware.
+
+    Attributes
+    ----------
+    mmcore: CMMCorePlus
+        The `CMMCorePlus` instance to use for hardware control.
+    use_hardware_sequencing : bool
+        Whether to use hardware sequencing if possible. If `True`, the engine will
+        attempt to combine MDAEvents into a single `SequencedEvent` if
+        [`core.canSequenceEvents()`][pymmcore_plus.CMMCorePlus.canSequenceEvents]
+        reports that the events can be sequenced.
     """
 
-    def __init__(self, mmc: CMMCorePlus) -> None:
+    def __init__(self, mmc: CMMCorePlus, use_hardware_sequencing: bool = True) -> None:
         self._mmc = mmc
+        self.use_hardware_sequencing = use_hardware_sequencing
 
         # used for one_shot autofocus to store the z correction for each position index.
         # map of {position_index: z_correction}
         self._z_correction: dict[int | None, float] = {}
 
-    # ===================== SETUP =====================
+    @property
+    def mmcore(self) -> CMMCorePlus:
+        """The `CMMCorePlus` instance to use for hardware control."""
+        return self._mmc
+
+    # ===================== Protocol Implementation =====================
 
     def setup_sequence(self, sequence: MDASequence) -> None:
         """Setup the hardware for the entire sequence.
 
         (currently, this does nothing but get the global `CMMCorePlus` singleton
-        if one is not already provided).
+        if one has not already been provided).
         """
-        from pymmcore_plus.core import CMMCorePlus
+        if not self._mmc:
+            from pymmcore_plus.core import CMMCorePlus
 
-        self._mmc = self._mmc or CMMCorePlus.instance()
+            self._mmc = CMMCorePlus.instance()
 
         # set sequence fov size for grid plans
         if hasattr(sequence, "set_fov_size"):
@@ -74,6 +87,84 @@ class MDAEngine(PMDAEngine):
         else:
             self.setup_single_event(event)
         self._mmc.waitForSystem()
+
+    def exec_event(self, event: MDAEvent) -> EventPayload | None:
+        """Execute an individual event and return the image data."""
+        action = getattr(event, "action", None)
+        if isinstance(action, HardwareAutofocus) and event.z_pos is not None:
+            try:
+                # execute hardware autofocus
+                new_correction = self._execute_autofocus(action)
+            except RuntimeError as e:
+                logger.warning("Hardware autofocus failed. {}", e)
+            else:
+                # store correction for this position index
+                p_idx = event.index.get("p", None)
+                self._z_correction[p_idx] = new_correction
+            return None
+
+        if isinstance(event, SequencedEvent):
+            return self.exec_sequenced_event(event)
+        else:
+            return self.exec_single_event(event)
+
+    def event_iterator(self, events: Iterable[MDAEvent]) -> Iterator[MDAEvent]:
+        """Event iterator that merges events for hardware sequencing if possible.
+
+        This wraps `for event in events: ...` inside `MDARunner.run()` and combines
+        sequenceable events into an instance of `SequencedEvent` if
+        `self.use_hardware_sequencing` is `True`.
+        """
+        if not self.use_hardware_sequencing:
+            yield from events
+            return
+
+        seq: list[MDAEvent] = []
+        for event in events:
+            # if the sequence is empty or the current event can be sequenced with the
+            # previous event, add it to the sequence
+            if not seq or self._mmc.canSequenceEvents(seq[-1], event, len(seq)):
+                seq.append(event)
+            else:
+                # otherwise, yield a SequencedEvent if the sequence has accumulated
+                # more than one event, otherwise yield the single event
+                yield seq[0] if len(seq) == 1 else SequencedEvent.create(seq)
+                # add this current event and start a new sequence
+                seq = [event]
+        # yield any remaining events
+        if seq:
+            yield seq[0] if len(seq) == 1 else SequencedEvent.create(seq)
+
+    # ===================== Regular Events =====================
+
+    def setup_single_event(self, event: MDAEvent) -> None:
+        """Setup hardware for a single (non-sequenced) event.
+
+        This method is not part of the PMDAEngine protocol (it is called by
+        `setup_event`, which *is* part of the protocol), but it is made public
+        in case a user wants to subclass this engine and override this method.
+        """
+        if event.x_pos is not None or event.y_pos is not None:
+            self._set_event_position(event)
+        if event.z_pos is not None:
+            self._set_event_z(event)
+
+        if event.channel is not None:
+            self._mmc.setConfig(event.channel.group, event.channel.config)
+        if event.exposure is not None:
+            self._mmc.setExposure(event.exposure)
+
+    def exec_single_event(self, event: MDAEvent) -> EventPayload | None:
+        """Execute a single (non-triggered) event and return the image data.
+
+        This method is not part of the PMDAEngine protocol (it is called by
+        `exec_event`, which *is* part of the protocol), but it is made public
+        in case a user wants to subclass this engine and override this method.
+        """
+        self._mmc.snapImage()
+        return EventPayload(image=self._mmc.getImage())
+
+    # ===================== Sequenced Events =====================
 
     def setup_sequenced_event(self, event: SequencedEvent) -> None:
         """Setup hardware for a sequenced (triggered) event.
@@ -122,45 +213,6 @@ class MDAEngine(PMDAEngine):
         elif event.channel is not None:
             core.setConfig(event.channel.group, event.channel.config)
 
-    def setup_single_event(self, event: MDAEvent) -> None:
-        """Setup hardware for a single (non-sequenced) event.
-
-        This method is not part of the PMDAEngine protocol (it is called by
-        `setup_event`, which *is* part of the protocol), but it is made public
-        in case a user wants to subclass this engine and override this method.
-        """
-        if event.x_pos is not None or event.y_pos is not None:
-            self._set_event_position(event)
-        if event.z_pos is not None:
-            self._set_event_z(event)
-
-        if event.channel is not None:
-            self._mmc.setConfig(event.channel.group, event.channel.config)
-        if event.exposure is not None:
-            self._mmc.setExposure(event.exposure)
-
-    # ===================== EXEC =====================
-
-    def exec_event(self, event: MDAEvent) -> EventPayload | None:
-        """Execute an individual event and return the image data."""
-        action = getattr(event, "action", None)
-        if isinstance(action, HardwareAFType) and event.z_pos is not None:
-            try:
-                # execute hardware autofocus
-                new_correction = self._execute_autofocus(action)  # type: ignore
-            except RuntimeError as e:
-                logger.warning("Hardware autofocus failed. {}", e)
-            else:
-                # store correction for this position index
-                p_idx = event.index.get("p", None)
-                self._z_correction[p_idx] = new_correction
-            return None
-
-        if isinstance(event, SequencedEvent):
-            return self.exec_sequenced_event(event)
-        else:
-            return self.exec_single_event(event)
-
     def exec_sequenced_event(self, event: SequencedEvent) -> EventPayload:
         """Execute a sequenced (triggered) event and return the image data.
 
@@ -185,6 +237,8 @@ class MDAEngine(PMDAEngine):
         images = []
         while self._mmc.isSequenceRunning():
             if self._mmc.getRemainingImageCount():
+                # TODO: pop with Metadata
+                # see https://github.com/pymmcore-plus/pymmcore-plus/issues/220
                 images.append(self._mmc.popNextImage())
             else:
                 time.sleep(0.001)
@@ -205,39 +259,7 @@ class MDAEngine(PMDAEngine):
 
         return EventPayload(image_sequence=images)
 
-    def exec_single_event(self, event: MDAEvent) -> EventPayload | None:
-        """Execute a single (non-triggered) event and return the image data.
-
-        This method is not part of the PMDAEngine protocol (it is called by
-        `exec_event`, which *is* part of the protocol), but it is made public
-        in case a user wants to subclass this engine and override this method.
-        """
-        self._mmc.snapImage()
-        return EventPayload(image=self._mmc.getImage())
-
     # ===================== EXTRA =====================
-
-    def event_iterator(self, events: Iterable[MDAEvent]) -> Iterator[MDAEvent]:
-        """Event iterator that merges events for hardware sequencing if possible.
-
-        This wraps `for event in events: ...` inside `MDARunner.run()` and combines
-        sequenceable events into an instance of `SequencedEvent`.
-        """
-        seq: list[MDAEvent] = []
-        for event in events:
-            # if the sequence is empty or the current event can be sequenced with the
-            # previous event, add it to the sequence
-            if not seq or self._mmc.canSequenceEvents(seq[-1], event, len(seq)):
-                seq.append(event)
-            else:
-                # otherwise, yield a SequencedEvent if the sequence has accumulated
-                # more than one event, otherwise yield the single event
-                yield seq[0] if len(seq) == 1 else SequencedEvent.create(seq)
-                # add this current event and start a new sequence
-                seq = [event]
-        # yield any remaining events
-        if seq:
-            yield seq[0] if len(seq) == 1 else SequencedEvent.create(seq)
 
     def _execute_autofocus(self, action: HardwareAutofocus) -> float:
         """Perform the hardware autofocus.
