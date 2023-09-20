735ae9e9e512eb96495a11c2523c85a32799b298
Talley Lambert <talley.lambert@gmail.com>
2023-09-20 15:17:09 -0400

    update

--- examples/zarr_mda.py
+++ examples/zarr_mda.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, MutableMapping, Sequence
+from typing import TYPE_CHECKING, MutableMapping, Sequence
 
 import useq
 from pymmcore_plus import CMMCorePlus, configure_logging
@@ -10,6 +10,7 @@ from useq import MDASequence
 if TYPE_CHECKING:
     import numpy as np
     import zarr
+
 
 POS_PREFIX = "p"
 AXTYPE = {
@@ -44,18 +45,57 @@ class OMEZarrHandler:
         self,
         store_or_group: MutableMapping | str | None | zarr.Group = None,
         overwrite: bool = False,
+        *,
+        synchronizer=None,
+        zarr_version=None,
     ) -> None:
         import zarr
 
         if isinstance(store_or_group, zarr.Group):
             self._group = store_or_group
         else:
-            self._group = zarr.group(store_or_group, overwrite=overwrite)
+            self._group = zarr.group(
+                store_or_group,
+                overwrite=overwrite,
+                synchronizer=synchronizer,
+                zarr_version=zarr_version,
+            )
 
         # we store a map of position index to zarr.Array
         # the group will have a dataset for each position
         self._arrays: dict[str, zarr.Array] = {}
         self._current_sequence: useq.MDASequence | None = None
+
+    def sequenceStarted(self, seq: useq.MDASequence) -> None:
+        self._current_sequence = seq
+        self._used_axes = tuple(x for x in self._current_sequence.used_axes if x != "p")
+
+    def sequenceFinished(self, seq: useq.MDASequence) -> None:
+        self._current_sequence = None
+
+    def frameReady(
+        self, frame: np.ndarray, event: useq.MDAEvent, meta: dict | None = None
+    ) -> None:
+        key = f'{POS_PREFIX}{event.index.get("p", 0)}'
+        if key not in self._arrays:
+            if not self._current_sequence:
+                self._current_sequence = event.sequence
+            if not (seq := self._current_sequence):
+                raise NotImplementedError(
+                    "Writing zarr without a MDASequence not yet implemented"
+                )
+            shape = (*tuple(v for k, v in seq.sizes.items() if k != "p"), *frame.shape)
+            axes = tuple(k for k in seq.sizes if k != "p")
+            ary = self._new_array(key, shape, frame.dtype, axes)
+        else:
+            ary = self._arrays[key]
+
+        index = tuple(event.index.get(k) for k in self._used_axes)
+        ary[index] = frame
+
+    @property
+    def group(self) -> zarr.Group:
+        return self._group
 
     def _new_array(
         self, key: str, shape: tuple[int, ...], dtype: np.dtype, axes: tuple[str, ...]
@@ -63,7 +103,9 @@ class OMEZarrHandler:
         # a chunk is a single XY plane
         chunks = [1] * len(shape)
         chunks[-2:] = shape[-2:]
-        ary = self._group.create(key, shape=shape, chunks=chunks, dtype=dtype)
+        ary = self._group.create(
+            key, shape=shape, chunks=chunks, dtype=dtype, dimension_separator="/"
+        )
         self._arrays[key] = ary
 
         scales = self._group.attrs.get("multiscales", [])
@@ -71,44 +113,6 @@ class OMEZarrHandler:
         self._group.attrs["multiscales"] = scales
 
         return ary
-
-    def sequenceStarted(self, seq: useq.MDASequence) -> None:
-        self._current_sequence = seq
-
-    #     scales = self._group.attrs.get("multiscales", [])
-    #     # create an array for each stage position
-    #     shape = (*tuple(v for k, v in seq.sizes.items() if k != "p"), 1, 1)
-    #     axes = tuple(k for k in seq.sizes if k != "p")
-    #     for i, pos in enumerate(seq.stage_positions):
-    #         # XXX I considered using position.name as the array key here, but then we
-    #         # risk a zarr.errors.ContainsArrayError if two positions have the same name
-    #         ary, created = self._get_or_create_position_array(
-    #             f"{POS_PREFIX}{i}",
-    #             shape=shape,
-    #             chunks=[1] * len(shape),  # add xy shape on first frame
-    #             dtype="<u2",  # TODO
-    #         )
-    #         if created:
-    #             scales.append(multiscales_image(ary.path, pos.name or ary.path, axes))
-    #     self._group.attrs["multiscales"] = scales
-
-    def frameReady(
-        self, frame: np.ndarray, event: useq.MDAEvent, meta: dict | None = None
-    ) -> None:
-        key = f'{POS_PREFIX}{event.index.get("p", 0)}'
-        if key not in self._arrays:
-            seq = self._current_sequence
-            shape = (*tuple(v for k, v in seq.sizes.items() if k != "p"), *frame.shape)
-            axes = tuple(k for k in seq.sizes if k != "p")
-            ary = self._new_array(key, shape, frame.dtype, axes)
-        else:
-            ary = self._arrays[key]
-
-        print(ary)
-        # index = tuple(event.index.get(k, 0) for k in ary.dims[:-2])
-
-    def arrays(self) -> dict[str, zarr.Array]:
-        return dict(self._group.arrays())
 
 
 sequence = MDASequence(
@@ -126,5 +130,5 @@ configure_logging(stderr_level="WARNING")
 with listeners_connected(core.mda.events, handler):
     core.mda.run(sequence)
 
-print([x.shape for x in handler.arrays().values()])
-print([x.chunks for x in handler.arrays().values()])
+print([x.shape for _, x in handler.group.arrays()])
+print([x.chunks for _, x in handler.group.arrays()])
