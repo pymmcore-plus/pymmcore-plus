b8ae3d69b161932521ea3d298873010e694f9a3b
Talley Lambert <talley.lambert@gmail.com>
2023-09-21 09:28:59 -0400

    add test

---
+++ tests/test_thread_relay.py
@@ -0,0 +1,43 @@
+import time
+from unittest.mock import Mock, call
+
+import numpy as np
+import useq
+from pymmcore_plus import CMMCorePlus
+from pymmcore_plus.mda import mda_listeners_connected
+
+
+def test_runner_cancel(core: CMMCorePlus) -> None:
+    mock = Mock()
+
+    class SlowHandler:
+        def frameReady(self, ary, event):
+            mock(event.index.get("t"))
+            time.sleep(0.1)
+
+    LOOPS = 4
+    seq = useq.MDASequence(time_plan=useq.TIntervalLoops(loops=LOOPS, interval=0))
+
+    handler = SlowHandler()
+
+    # by default, we wait for the relay to finish
+    # so the mock should be called LOOPS times
+    with mda_listeners_connected(handler, mda_events=core.mda.events):
+        core.mda.run(seq)
+    assert mock.call_count == LOOPS
+    mock.assert_has_calls([call(t) for t in range(LOOPS)])
+
+    # with wait_on_exit=False, the mock should be called less than LOOPS times
+    # because the relay is stopped before SlowHandler finishes
+    mock.reset_mock()
+    with mda_listeners_connected(
+        handler, mda_events=core.mda.events, wait_on_exit=False
+    ):
+        core.mda.run(seq)
+
+    assert mock.call_count < LOOPS
+
+    # make sure it got disconnected
+    mock.reset_mock()
+    core.mda.events.frameReady.emit(np.empty((10, 10)), useq.MDAEvent(), {})
+    mock.assert_not_called()
