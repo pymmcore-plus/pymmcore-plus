c295150d8c9632d86b29d1d2d70d3cd4af41239d
Talley Lambert <talley.lambert@gmail.com>
2023-09-21 12:33:53 -0400

    update test

--- tests/test_thread_relay.py
+++ tests/test_thread_relay.py
@@ -1,24 +1,22 @@
-from typing import TYPE_CHECKING
-from unittest.mock import Mock
+import time
+from unittest.mock import Mock, call
 
+import numpy as np
 import useq
 from pymmcore_plus import CMMCorePlus
 from pymmcore_plus.mda import mda_listeners_connected
 
-if TYPE_CHECKING:
-    from pytestqt.qtbot import QtBot
 
-
-def test_mda_listeners_connected(core: CMMCorePlus, qtbot: "QtBot") -> None:
+def test_mda_listeners_connected(core: CMMCorePlus) -> None:
     mock = Mock()
 
     class SlowHandler:
-        def frameReady(self, ary, event):
+        def frameReady(self, ary: np.ndarray, event: useq.MDAEvent):
             mock(event.index.get("t"))
-            # time.sleep(0.1)
+            time.sleep(0.05)
 
     LOOPS = 4
-    seq = useq.MDASequence(time_plan=useq.TIntervalLoops(loops=LOOPS, interval=0.01))
+    seq = useq.MDASequence(time_plan=useq.TIntervalLoops(loops=LOOPS, interval=0))
 
     handler = SlowHandler()
 
@@ -28,19 +26,26 @@ def test_mda_listeners_connected(core: CMMCorePlus, qtbot: "QtBot") -> None:
         core.mda.run(seq)
 
     assert mock.call_count == LOOPS
-    # mock.assert_has_calls([call(t) for t in range(LOOPS)])
+    mock.assert_has_calls([call(t) for t in range(LOOPS)])
 
-    # # with wait_on_exit=False, the mock should be called less than LOOPS times
-    # # because the relay is stopped before SlowHandler finishes
-    # mock.reset_mock()
-    # with mda_listeners_connected(
-    #     handler, mda_events=core.mda.events, wait_on_exit=False
-    # ):
-    #     with qtbot.waitSignal(core.mda.events.sequenceFinished):
-    #         core.mda.run(seq)
-    # assert mock.call_count < LOOPS
+    # with wait_on_exit=False, the mock should be called less than LOOPS times
+    # because the relay is stopped before SlowHandler finishes
+    mock.reset_mock()
+    with mda_listeners_connected(
+        handler, mda_events=core.mda.events, wait_on_exit=False
+    ):
+        core.mda.run(seq)
+    assert mock.call_count < LOOPS
 
-    # # make sure it got disconnected
-    # mock.reset_mock()
-    # core.mda.events.frameReady.emit(np.empty((10, 10)), useq.MDAEvent(), {})
-    # mock.assert_not_called()
+    # make sure it got disconnected
+    mock.reset_mock()
+    core.mda.events.frameReady.emit(np.empty((10, 10)), useq.MDAEvent(), {})
+    mock.assert_not_called()
+
+    # with asynchronous=False, it reduces to a synchronous listeners_connected context
+    mock.reset_mock()
+    with mda_listeners_connected(
+        handler, mda_events=core.mda.events, asynchronous=False
+    ):
+        core.mda.run(seq)
+    assert mock.call_count == LOOPS
