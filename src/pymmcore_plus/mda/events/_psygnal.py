e08cd7c1765355e6c78387f6f4a0d9af9347047c
Talley Lambert <talley.lambert@gmail.com>
2023-09-24 14:34:19 -0400

    feat: emit summary metadata on sequence start

--- src/pymmcore_plus/mda/events/_psygnal.py
+++ src/pymmcore_plus/mda/events/_psygnal.py
@@ -4,7 +4,7 @@ from useq import MDAEvent, MDASequence
 
 
 class MDASignaler:
-    sequenceStarted = Signal(MDASequence)  # at the start of an MDA sequence
+    sequenceStarted = Signal(MDASequence, dict)  # at the start of an MDA sequence
     sequencePauseToggled = Signal(bool)  # when MDA is paused/unpaused
     sequenceCanceled = Signal(MDASequence)  # when mda is canceled
     sequenceFinished = Signal(MDASequence)  # when mda is done (whether canceled or not)
