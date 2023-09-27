a3492c6a15b6e634b9c316e7ed4318035d67032c
Talley Lambert <talley.lambert@gmail.com>
2023-09-27 13:24:55 -0400

    feat: ImageSequenceWriter (#267)
    * tests
    
    * update docs
    
    * rename
    
    * more rename
    
    * rename again
    
    * fix: add sub-sequence
    
    * fix: rename
    
    * test: update
    
    * fix: get_full_sequence_axes
    
    * Update src/pymmcore_plus/mda/handlers/_tiff_sequence_writer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * Update src/pymmcore_plus/mda/handlers/_tiff_sequence_writer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * Update tests/test_handlers/test_tiff_series_writer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * Update examples/tiff_sequence_mda.py
    
    * style(pre-commit.ci): auto fixes [...]
    
    * test: update test
    
    * update file name
    
    * make generic writer
    
    ---------
    
    Co-authored-by: fdrgsp <federico.gasparoli@gmail.com>
    Co-authored-by: federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/img_sequence_mda.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from pymmcore_plus import CMMCorePlus
+from pymmcore_plus.mda import mda_listeners_connected
+from pymmcore_plus.mda.handlers import ImageSequenceWriter
+from useq import MDASequence, Position
+
+core = CMMCorePlus.instance()
+core.loadSystemConfiguration()
+
+sequence = MDASequence(
+    channels=["DAPI", {"config": "FITC", "exposure": 1}],
+    stage_positions=[
+        {"x": 1, "y": 1, "name": "some position"},
+        Position(
+            x=2, y=2, z=3, sequence=MDASequence(grid_plan={"rows": 2, "columns": 2})
+        ),
+    ],
+    time_plan={"interval": 2, "loops": 2},
+    z_plan={"range": 2, "step": 0.5},
+    axis_order="tpcz",
+)
+
+# requires tifffile
+tiff_writer = ImageSequenceWriter(
+    "data_folder", overwrite=True, prefix="test", include_frame_count=True
+)
+
+# requires imageio
+png_writer = ImageSequenceWriter("data_folderp", extension=".png", overwrite=True)
+
+with mda_listeners_connected(tiff_writer, png_writer):
+    core.mda.run(sequence)
