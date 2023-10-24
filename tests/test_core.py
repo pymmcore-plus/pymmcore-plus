11a7cc4cdc405862c7b80139913b4dccee8e8db2
Talley Lambert <talley.lambert@gmail.com>
2023-10-24 11:16:26 -0400

    feat: add systemConfigurationFile to retrieve last config path (#277)

--- tests/test_core.py
+++ tests/test_core.py
@@ -64,6 +64,7 @@ def test_load_system_config(core: CMMCorePlus):
 
     config_path = Path(__file__).parent / "local_config.cfg"
     core.loadSystemConfiguration(str(config_path))
+    assert core.systemConfigurationFile() == str(config_path)
     assert core.getLoadedDevices() == (
         "DHub",
         "Camera",
