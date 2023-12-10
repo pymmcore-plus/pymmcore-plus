78585577b50d12d30c9ae33c53e4fe447df651e6
Talley Lambert <talley.lambert@gmail.com>
2023-12-10 12:52:42 -0500

    feat: OMEZarr writer (#263)
    * wip
    
    * remove group
    
    * update
    
    * more
    
    * tmp
    
    * tpm
    
    * thread relay
    
    * feat: add mda thread relay
    
    * add test
    
    * rename test
    
    * fix qt
    
    * change syntax
    
    * add fix
    
    * update test
    
    * move file
    
    * comment
    
    * add test
    
    * fix qt
    
    * change syntax
    
    * update test
    
    * merge changes
    
    * update docs
    
    * test
    
    * pragma
    
    * move test
    
    * add zarr to testing
    
    * rename test
    
    * more tests
    
    * fix typing

---
+++ examples/zarr_mda.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from pymmcore_plus import CMMCorePlus
+from pymmcore_plus.mda import mda_listeners_connected
+from pymmcore_plus.mda.handlers import OMEZarrWriter
+from useq import MDASequence
+
+core = CMMCorePlus.instance()
+core.loadSystemConfiguration()
+
+sequence = MDASequence(
+    channels=["DAPI", {"config": "FITC", "exposure": 1}],
+    # stage_positions=[{"x": 1, "y": 1, "name": "some position"}, {"x": 0, "y": 0}],
+    time_plan={"interval": 2, "loops": 2},
+    z_plan={"range": 4, "step": 0.5},
+    axis_order="tpcz",
+)
+
+# use OMEZarrHandler("file.zarr") to write to a directory
+# use OMEZarrHandler.in_tmpdir() to write to a temporary directory
+# pass None or no arguments to write to Memory
+writer = OMEZarrWriter("file.zarr", overwrite=True, minify_attrs_metadata=True)
+
+with mda_listeners_connected(writer):
+    core.mda.run(sequence)
