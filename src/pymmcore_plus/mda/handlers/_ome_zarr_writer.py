2303ae86b418d275f18fa703aae53942d203e329
Willi L Stepp <52414717+wl-stepp@users.noreply.github.com>
2023-12-22 16:41:59 +0100

    fix: Zarr part mda (#302)
    * failing for part mda
    
    * pre-commit
    
    * limit indeces in zarr
    
    * style(pre-commit.ci): auto fixes [...]
    
    * pre-commit
    
    * correct axes metadata for reading with
    napari-ome-zarr
    
    * Update tests/test_handlers/test_ome_zarr.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * style(pre-commit.ci): auto fixes [...]
    
    * remove space
    
    * style(pre-commit.ci): auto fixes [...]
    
    * Update src/pymmcore_plus/mda/handlers/_ome_zarr_writer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- src/pymmcore_plus/mda/handlers/_ome_zarr_writer.py
+++ src/pymmcore_plus/mda/handlers/_ome_zarr_writer.py
@@ -228,8 +228,8 @@ class OMEZarrWriter:
 
             # create the new array, getting XY chunksize from the frame
             # and total shape from the sequence
-            shape = (*tuple(v for k, v in seq.sizes.items() if k != "p"), *frame.shape)
-            axes = (*(k for k in seq.sizes if k != "p"), "y", "x")
+            axes = (*(k for k in seq.used_axes if k != "p"), "y", "x")
+            shape = (*tuple(v for k, v in seq.sizes.items() if k in axes), *frame.shape)
             ary = self._new_array(key, shape, frame.dtype, axes)
 
             # write the MDASequence metadata and xarray _ARRAY_DIMENSIONS to the array
--- tests/test_handlers/test_ome_zarr.py
+++ tests/test_handlers/test_ome_zarr.py
@@ -15,17 +15,22 @@ if TYPE_CHECKING:
 else:
     zarr = pytest.importorskip("zarr")
 
+part_mda = useq.MDASequence(
+    channels=["Cy5", "FITC"],
+    stage_positions=[(222, 1, 1), (111, 0, 0)],
+    time_plan={"interval": 0.1, "loops": 3},
+)
 
-@pytest.mark.parametrize("store", ["out.zarr", None, "tmp"])
-def test_ome_zarr_writer(store: str | None, tmp_path: Path, core: CMMCorePlus) -> None:
-    mda = useq.MDASequence(
-        channels=["Cy5", "FITC"],
-        time_plan={"interval": 0.1, "loops": 3},
-        stage_positions=[(222, 1, 1), (111, 0, 0)],
-        z_plan={"range": 0.3, "step": 0.1},
-        axis_order="tpcz",
-    )
+full_mda = part_mda.replace(axis_order="tpcz", channels=["Cy5", "FITC"])
 
+
+@pytest.mark.parametrize(
+    "store, mda",
+    [("out.zarr", full_mda), (None, full_mda), ("tmp", full_mda), (None, part_mda)],
+)
+def test_ome_zarr_writer(
+    store: str | None, mda: useq.MDASequence, tmp_path: Path, core: CMMCorePlus
+) -> None:
     if store == "tmp":
         writer = OMEZarrWriter.in_tmpdir()
     elif store is None:
@@ -36,7 +41,7 @@ def test_ome_zarr_writer(store: str | None, tmp_path: Path, core: CMMCorePlus) -
     with mda_listeners_connected(writer, mda_events=core.mda.events):
         core.mda.run(mda)
 
-    no_p_shape = tuple(v for k, v in mda.sizes.items() if k != "p")
+    no_p_shape = tuple(v for k, v in mda.sizes.items() if k != "p" and v > 0)
     expected_shape = (*no_p_shape, 512, 512)
 
     actual_shapes = {k: v.shape for k, v in writer.group.arrays()}
