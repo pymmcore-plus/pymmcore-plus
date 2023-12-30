53dce1e389cf2a0fb4f612024d80166d0e175258
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2023-12-30 11:02:17 -0500

    feat: update OMEZarrWriter to position-specific sub-MDAsequences (#299)
    * feat: updatre OMEZarrWriter to handle sub-sequences
    
    * test: update tests
    
    * fix: sequenceStarted
    
    * fix: fix comment
    
    * fix: remove unused
    
    * refactor: extract methods
    
    * style(pre-commit.ci): auto fixes [...]
    
    * refactor: rename
    
    * fic: docstring
    
    * fix: ValueError message
    
    * update tests
    
    * fix: update with position_sizes method
    
    * style(pre-commit.ci): auto fixes [...]
    
    * fix: comment
    
    * fix: remove comment
    
    * update func
    
    * fix expectation
    
    * fix: update _ARRAY_DIMENSIONS
    
    * reduce diff
    
    * slight cleanup and tests
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- tests/test_handlers/test_ome_zarr.py
+++ tests/test_handlers/test_ome_zarr.py
@@ -15,21 +15,69 @@ if TYPE_CHECKING:
 else:
     zarr = pytest.importorskip("zarr")
 
-part_mda = useq.MDASequence(
+
+SIMPLE_MDA = useq.MDASequence(
+    channels=["Cy5", "FITC"],
+    time_plan={"interval": 0.1, "loops": 2},
+    axis_order="tpcz",
+)
+SIMPLE_EXPECTATION = {"p0": {"t": 2, "c": 2, "y": 512, "x": 512}}
+
+MULTIPOINT_MDA = SIMPLE_MDA.replace(
     channels=["Cy5", "FITC"],
     stage_positions=[(222, 1, 1), (111, 0, 0)],
-    time_plan={"interval": 0.1, "loops": 3},
+    time_plan={"interval": 0.1, "loops": 2},
 )
+MULTIPOINT_EXPECTATION = {
+    "p0": {"t": 2, "c": 2, "y": 512, "x": 512},
+    "p1": {"t": 2, "c": 2, "y": 512, "x": 512},
+}
 
-full_mda = part_mda.replace(axis_order="tpcz", channels=["Cy5", "FITC"])
+FULL_MDA = MULTIPOINT_MDA.replace(z_plan={"range": 0.3, "step": 0.1})
+FULL_EXPECTATION = {
+    "p0": {"t": 2, "c": 2, "z": 4, "y": 512, "x": 512},
+    "p1": {"t": 2, "c": 2, "z": 4, "y": 512, "x": 512},
+}
+
+COMPLEX_MDA = FULL_MDA.replace(
+    channels=["Cy5"],
+    time_plan={"interval": 0.1, "loops": 3},
+    stage_positions=[
+        (222, 1, 1),
+        {
+            "x": 0,
+            "y": 0,
+            "sequence": useq.MDASequence(
+                grid_plan={"rows": 2, "columns": 1},
+                z_plan={"range": 3, "step": 1},
+            ),
+        },
+    ],
+)
+COMPLEX_EXPECTATION = {
+    "p0": {"t": 3, "c": 1, "z": 4, "y": 512, "x": 512},
+    "p1": {"t": 3, "g": 2, "c": 1, "z": 4, "y": 512, "x": 512},
+}
 
 
 @pytest.mark.parametrize(
-    "store, mda",
-    [("out.zarr", full_mda), (None, full_mda), ("tmp", full_mda), (None, part_mda)],
+    "store, mda, expected_shapes",
+    [
+        (None, SIMPLE_MDA, SIMPLE_EXPECTATION),
+        (None, MULTIPOINT_MDA, MULTIPOINT_EXPECTATION),
+        (None, FULL_MDA, FULL_EXPECTATION),
+        ("out.zarr", FULL_MDA, FULL_EXPECTATION),
+        (None, FULL_MDA, FULL_EXPECTATION),
+        ("tmp", FULL_MDA, FULL_EXPECTATION),
+        (None, COMPLEX_MDA, COMPLEX_EXPECTATION),
+    ],
 )
 def test_ome_zarr_writer(
-    store: str | None, mda: useq.MDASequence, tmp_path: Path, core: CMMCorePlus
+    store: str | None,
+    mda: useq.MDASequence,
+    expected_shapes: dict[str, dict],
+    tmp_path: Path,
+    core: CMMCorePlus,
 ) -> None:
     if store == "tmp":
         writer = OMEZarrWriter.in_tmpdir()
@@ -41,17 +89,23 @@ def test_ome_zarr_writer(
     with mda_listeners_connected(writer, mda_events=core.mda.events):
         core.mda.run(mda)
 
-    no_p_shape = tuple(v for k, v in mda.sizes.items() if k != "p" and v > 0)
-    expected_shape = (*no_p_shape, 512, 512)
-
-    actual_shapes = {k: v.shape for k, v in writer.group.arrays()}
-    assert actual_shapes == {"p0": expected_shape, "p1": expected_shape}
-
     if store:
-        # check that non-memory stores were written to disk
+        # ensure that non-memory stores were written to disk
         data = zarr.open(writer.group.store.path)
-        actual_shapes = {k: v.shape for k, v in data.arrays()}
-        assert actual_shapes == {"p0": expected_shape, "p1": expected_shape}
+        for _, ary in data.arrays():
+            # ensure real data was written
+            assert ary.nchunks_initialized > 0
+            assert ary[0, 0].mean() > ary.fill_value
+    else:
+        data = writer.group
 
-        p0 = data["p0"]
-        assert p0[0, 0, 0].mean() > p0.fill_value  # real data was written
+    # check that arrays have expected shape and dimensions
+    actual_shapes = {
+        k: dict(zip(v.attrs["_ARRAY_DIMENSIONS"], v.shape)) for k, v in data.arrays()
+    }
+    assert actual_shapes == expected_shapes
+
+    # check that the MDASequence was stored
+    for _, v in data.arrays():
+        stored_seq = useq.MDASequence.parse_obj(v.attrs["useq_MDASequence"])
+        assert stored_seq == mda
