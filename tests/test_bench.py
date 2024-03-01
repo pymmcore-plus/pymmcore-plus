caa0dd016eb88cc83375a51d3cddb260a0cb2efd
Talley Lambert <talley.lambert@gmail.com>
2024-03-01 12:10:52 -0500

    try fix bench

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -14,7 +14,7 @@ if all(x not in {"--codspeed", "tests/test_bench.py"} for x in sys.argv):
 
 MIN_EXPOSURE = 0.001
 T10 = useq.TIntervalLoops(interval=0, loops=10)  # type: ignore
-T40 = useq.TIntervalLoops(interval=0, loops=40)  # type: ignore
+T20 = useq.TIntervalLoops(interval=0, loops=20)  # type: ignore
 T200 = useq.TIntervalLoops(interval=0, loops=200)  # type: ignore
 DAPI = useq.Channel(config="DAPI", exposure=MIN_EXPOSURE)
 FITC = useq.Channel(config="FITC", exposure=MIN_EXPOSURE)
@@ -23,7 +23,7 @@ CY5 = useq.Channel(config="Cy5", exposure=MIN_EXPOSURE)
 C1 = (DAPI,)
 C4 = (DAPI, FITC, RHOD, CY5)
 P1 = (useq.Position(x=0, y=0, z=0),)
-P20 = tuple(useq.Position(x=i, y=i, z=i) for i in range(20))
+P10 = tuple(useq.Position(x=i, y=i, z=i) for i in range(10))
 P100 = tuple(useq.Position(x=i, y=i, z=i) for i in range(100))
 Z10 = useq.ZRangeAround(range=10, step=1)
 Z40 = useq.ZRangeAround(range=40, step=1)
@@ -45,8 +45,8 @@ MDAS = {
     "z10c1p1t10": useq.MDASequence(
         z_plan=Z10, time_plan=T10, channels=C1, stage_positions=P1, axis_order="zcpt"
     ),
-    "t40p20c4z40": useq.MDASequence(
-        z_plan=Z40, time_plan=T40, channels=C4, stage_positions=P20, axis_order="tpcz"
+    "t40p10c4z40": useq.MDASequence(
+        z_plan=Z40, time_plan=T20, channels=C4, stage_positions=P10, axis_order="tpcz"
     ),
 }
 
