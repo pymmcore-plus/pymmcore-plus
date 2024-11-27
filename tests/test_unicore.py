dbaeb06b3e62102a381d4126363f307b0e143ced
Talley Lambert <talley.lambert@gmail.com>
2024-11-27 12:10:04 -0500

    more test

--- tests/test_unicore.py
+++ tests/test_unicore.py
@@ -1,3 +1,5 @@
+from typing import ClassVar
+
 from pymmcore_plus.unicore import UniMMCore
 from pymmcore_plus.unicore._xy_stage_device import XYStageDevice
 
@@ -7,21 +9,22 @@ class MyStage(XYStageDevice):
 
     STOPPED = False
     HOMED = False
-    _pos = (0.0, 0.0)
+    ORIGIN: ClassVar[list[float]] = [0.0, 0.0]
+    _pos: ClassVar[list[float]] = [0.0, 0.0]
 
     def set_position_um(self, x: float, y: float) -> None:
         """Set the position of the stage."""
-        self._pos = (x, y)
+        self._pos[:] = [x, y]
 
     def get_position_um(self) -> tuple[float, float]:
         """Get the current position of the stage."""
-        return self._pos
+        return tuple(self._pos)  # type: ignore
 
     def set_origin_x(self) -> None:
-        pass
+        self._pos[0], self.ORIGIN[0] = 0.0, self._pos[0]
 
     def set_origin_y(self) -> None:
-        pass
+        self._pos[1], self.ORIGIN[1] = 0.0, self._pos[1]
 
     def stop(self) -> None:
         self.STOPPED = True
@@ -45,15 +48,20 @@ def test_unicore():
     # set the core XY stage device to the python device, dropping the C-side device
     core.setXYStageDevice("pyXY")
     assert core.getXYStageDevice() == "pyXY"
-    core.setXYPosition(10, 20)
-    assert core.getXYPosition() == (10, 20)
+    NEW_POS = (10, 20)
+    core.setXYPosition(*NEW_POS)
+    assert core.getXYPosition() == NEW_POS
 
     # can still set and query the C-side device directly
     core.waitForDevice("XY")
     core.setXYPosition("XY", 1.5, 3.7)
     x, y = core.getXYPosition("XY")
     assert (round(x, 1), round(y, 1)) == (1.5, 3.7)
-    assert core.getXYPosition("pyXY") == (10, 20)
+    assert core.getXYPosition("pyXY") == NEW_POS
+
+    core.setOriginXY("pyXY")
+    assert core.getXYPosition("pyXY") == (0, 0)
+    assert tuple(stage.ORIGIN) == NEW_POS
 
     core.home("pyXY")
     core.stop("pyXY")
