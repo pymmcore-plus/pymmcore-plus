ccfead95089b7b0cdd4a17419ca638f20e8d5613
Talley Lambert <talley.lambert@gmail.com>
2025-02-16 08:37:47 -0500

    fix: fix popNextImageAndMD

--- tests/test_core.py
+++ tests/test_core.py
@@ -339,18 +339,24 @@ def test_new_metadata():
     assert isinstance(md, pymmcore.Metadata)
 
 
-def test_md_(core: CMMCorePlus) -> None:
+def test_get_image_and_meta(core: CMMCorePlus) -> None:
     core.startContinuousSequenceAcquisition(10)
     core.stopSequenceAcquisition()
 
     image, md = core.getNBeforeLastImageAndMD(0)
-    assert isinstance(image, np.ndarray) and isinstance(md, Metadata)
+    assert isinstance(image, np.ndarray)
+    assert isinstance(md, Metadata)
+    assert bool(dict(md))
 
     image, md = core.getLastImageAndMD()
-    assert isinstance(image, np.ndarray) and isinstance(md, Metadata)
+    assert isinstance(image, np.ndarray)
+    assert isinstance(md, Metadata)
+    assert bool(dict(md))
 
     image, md = core.popNextImageAndMD()
-    assert isinstance(image, np.ndarray) and isinstance(md, Metadata)
+    assert isinstance(image, np.ndarray)
+    assert isinstance(md, Metadata)
+    assert bool(dict(md))
 
 
 def test_configuration(core: CMMCorePlus) -> None:
