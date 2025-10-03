490b2210fdf9ccc8272d6f08ed2e2209b51ef584
fdrgsp <federico.gasparoli@gmail.com>
2025-10-03 17:06:27 -0400

    wip

--- tests/test_metadata_to_ome.py
+++ tests/test_metadata_to_ome.py
@@ -38,6 +38,21 @@ PLATE_SEQ = useq.MDASequence(
     ),
 )
 
+PLATE_SEQ_FOVS = useq.MDASequence(
+    axis_order="pcz",
+    stage_positions=useq.WellPlatePlan(
+        plate=useq.WellPlate.from_str("96-well"),
+        a1_center_xy=(0, 0),
+        selected_wells=((0, 0, 0), (0, 1, 2)),
+        well_points_plan=useq.GridRowsColumns(rows=2, columns=2),
+    ),
+    z_plan=useq.ZRangeAround(range=3.0, step=1.0),
+    channels=(
+        useq.Channel(config="DAPI", exposure=20),
+        useq.Channel(config="FITC", exposure=30),
+    ),
+)
+
 GRID_SEQ = useq.MDASequence(
     time_plan=useq.TIntervalLoops(interval=0.5, loops=2),
     stage_positions=(
@@ -85,7 +100,10 @@ def _get_expected_images(seq: useq.MDASequence) -> int:
     return expected_images
 
 
-@pytest.mark.parametrize("seq", [BASIC_SEQ, PLATE_SEQ, GRID_SEQ, SEQ_WITH_SUBSEQ_GRID])
+@pytest.mark.parametrize(
+    # "seq", [BASIC_SEQ, PLATE_SEQ, PLATE_SEQ_FOVS, GRID_SEQ, SEQ_WITH_SUBSEQ_GRID]
+    "seq", [PLATE_SEQ, PLATE_SEQ_FOVS]
+)
 def test_ome_generation(seq: useq.MDASequence) -> None:
     mmc = CMMCorePlus()
     mmc.loadSystemConfiguration("tests/local_config.cfg")
