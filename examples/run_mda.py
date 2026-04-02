49dd65adf2d4194555ecfbed6b21ad88816bf654
Talley Lambert <talley.lambert@gmail.com>
2026-04-02 08:19:52 -0400

    feat: add summary metadata to tiff and zarr outputs (#602)
    * feat: add summary metadata serialization and integration for OME-TIFF and OME-Zarr
    
    * feat: enhance summary metadata functions to include property details option
    
    * Refactor OME metadata handling and improve system information retrieval
    
    - Removed the _ome.py module and integrated its functionality into the existing structure.
    - Updated system_info function to include backend information and adjusted property details handling.
    - Enhanced the SystemInfo schema to include backend details and mark certain fields as not required.
    - Refactored tests to accommodate changes in OME metadata handling and updated imports accordingly.
    - Removed deprecated test file test_metadata_to_ome.py and ensured relevant tests are maintained.
    
    * fix: update SystemInfo to mark pymmcore_backend as not required

--- examples/run_mda.py
+++ examples/run_mda.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from useq import MDAEvent, MDASequence
 
@@ -5,8 +7,8 @@ from pymmcore_plus import CMMCorePlus
 
 # see https://pymmcore-plus.github.io/useq-schema/api/ (1)
 sequence = MDASequence(
-    channels=["DAPI", {"config": "FITC", "exposure": 50}],
-    time_plan={"interval": 2, "loops": 5},
+    channels=[{"config": "DAPI", "exposure": 10}, {"config": "FITC", "exposure": 50}],
+    time_plan={"interval": 1, "loops": 5},
     z_plan={"range": 4, "step": 0.5},
     axis_order="tpcz",
 )
@@ -28,5 +30,12 @@ def on_start(sequence: MDASequence):
 
 mmc.mda.events.sequenceStarted.connect(on_start)
 
+# parse command line arguments to determine output format, default is no output.
+output = None
+if "--tiff" in sys.argv:
+    output = "example_mda.ome.tiff"
+elif "--zarr" in sys.argv:
+    output = "example_mda.ome.zarr"
+
 # run the sequence (5)
-mmc.mda.run(sequence)
+mmc.mda.run(sequence, output=output, overwrite=True)
